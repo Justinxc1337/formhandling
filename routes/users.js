a3243d7095260a43ba2a1b2aedde1989fa03beae
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-11-05 14:18:12 +0100

    retest

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', function(req, res, next){
+router.post('/users', function(req, res, next){
   if ((req.body.username == 'Jens') && (req.body.password == 'Gut')) {
     res.send('logget ind ' + req.body.username + " " + req.body.password);
   }

89171444979177a39bd3873ac761bcd41008b2b7
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-11-05 14:16:01 +0100

    test

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/users', function(req, res, next){
-  res.send('logget ind');
+router.post('/', function(req, res, next){
+  if ((req.body.username == 'Jens') && (req.body.password == 'Gut')) {
+    res.send('logget ind ' + req.body.username + " " + req.body.password);
+  }
+  else {
+    res.send('forkert username eller password');
+  }
 });
 
 module.exports = router;
